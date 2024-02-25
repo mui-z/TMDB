5f9915396f0fecf8b1885235f96decbd1a230acc
mui-z <93278577+mui-z@users.noreply.github.com>
2024-02-25 23:14:22 +0900

    append logger

--- TmdbPackage/Package.swift
+++ TmdbPackage/Package.swift
@@ -15,13 +15,24 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.8.2"),
+    .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "TmdbPackage",
-      dependencies: []),
+      dependencies: [
+        .product(
+          name: "ComposableArchitecture",
+          package: "swift-composable-architecture"
+        ),
+        .product(
+          name: "Logging",
+          package: "swift-log"
+        ),
+      ]    ),
     .testTarget(
       name: "TmdbPackageTests",
       dependencies: ["TmdbPackage"]),
---
+++ TmdbPackage/Sources/TmdbPackage/Utilities/Logging.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by osushi on 2024/02/25.
+//
+
+import Logging
+
+let logger = Logger(label: "studio.sekai.tmdb")
